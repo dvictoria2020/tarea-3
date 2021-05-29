d31bb477015381e20af766a38a22faa3601424c2
dvictoria2020 <vicdelfer@gmail.com>
2021-05-29 15:37:09 -0600

    commit inicial

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 // Mapa Leaflet
-var mapa = L.map('mapid').setView([9.95, -84.15], 13);
+var mapa = L.map('mapid').setView([9.94, -84.13], 13);
 
 // Definición de capas base
 var capa_osm = L.tileLayer(
@@ -9,6 +9,13 @@ var capa_osm = L.tileLayer(
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   }
 ).addTo(mapa);	
+
+var capa_topografico = L.tileLayer(
+  'https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', 
+  {
+	maxZoom: 19,
+  }
+).addTo(mapa);
 
 // Otra capa base esri
 var capa_esri = L.tileLayer(
@@ -20,8 +27,10 @@ var capa_esri = L.tileLayer(
 
 // Conjunto de capas base
 var capas_base = {
-  "ESRI": capa_esri,
   "OSM": capa_osm,
+  "Topografico": capa_topografico,
+  "ESRI": capa_esri
+
 };
 	    
 // Control de capas
@@ -30,37 +39,48 @@ control_capas = L.control.layers(capas_base).addTo(mapa);
 // Control de escala
 L.control.scale().addTo(mapa);
 
-// Capa vectorial de terrenos del estado en formato GeoJSON
-$.getJSON("https://dvictoria2020.github.io/tarea-3/tarea-3/datos/terrenos/terrenos_estado.geojson", function(geodata) {
-  var terrenos = L.geoJson(geodata, {
-    style: function(feature) {
-	  return {'color': "red", 'weight': 1.5, 'fillOpacity': 0.0}
-    },
-    onEachFeature: function(feature, layer) {
-      var popupText = "<strong>Propietario</strong>: " + feature.properties.nom_juridi + "<br>" + "<strong>Tipo de Inmueble</strong>: " + feature.properties.t_inmueb + "<br>" + "<strong>Clasificación</strong>: " + feature.properties.clasific + "<br>" + "<strong>Amenazas</strong>: " + feature.properties.r_fisica + "<br>" + "<strong>Número de finca</strong>: " + feature.properties.finca;
-      layer.bindPopup(popupText);
-    }			
+// Capa de coropletas  terrenos del estado en formato GeoJSON
+$.getJSON('https://dvictoria2020.github.io/tarea-3/datos/terrenos/terrenos_estado.geojson', function (geojson) {
+  var capa_terrenos = L.choropleth(geojson, {
+	    valueProperty: 'area',
+	    scale: ['white', 'blue'],
+	    steps: 7,
+	    mode: 'q',
+	    style: {
+	      color: '#fff',
+	      weight: 0.5,
+	      fillOpacity: 0.5
+	    }, 
+      onEachFeature: function(feature, layer) {
+        var popupText = "<strong>Propietario</strong>: " + feature.properties.nom_juridi + "<br>" + "<strong>Tipo de Inmueble</strong>: " + feature.properties.t_inmueb +  "<br>" + "<strong>Amenazas</strong>: " + feature.properties.r_fisica + "<br>" + "<strong>Número de finca</strong>: " + feature.properties.finca;
+        layer.bindPopup(popupText);
+      }
   }).addTo(mapa);
+  control_capas.addOverlay(capa_terrenos, 'Terrenos del Estado');
 
-  control_capas.addOverlay(terrenos, 'Terrenos del Estado');
+  // Leyenda de la capa de coropletas
+  var leyenda = L.control({ position: 'bottomleft' })
+  leyenda.onAdd = function (mapa) {
+    var div = L.DomUtil.create('div', 'info legend')
+    var limits = capa_terrenos.options.limits
+    var colors = capa_terrenos.options.colors
+    var labels = []
+
+    // Add min & max
+    div.innerHTML = '<div class="labels"><div class="min">' + limits[0] + '</div> \
+            <div class="max">' + limits[limits.length - 1] + '</div></div>'
+
+    limits.forEach(function (limit, index) {
+      labels.push('<li style="background-color: ' + colors[index] + '"></li>')
+    })
+
+    div.innerHTML += '<ul>' + labels.join('') + '</ul>'
+    return div
+    }
+    leyenda.addTo(mapa)
 });
 
-// Capa vectorial de terrenos del estado en formato GeoJSON
-$.getJSON("https://dvictoria2020.github.io/tarea-3/tarea-3/datos/terrenos/terrenos_estado2.geojson", function(geodata) {
-  var terrenos = L.geoJson(geodata, {
-    style: function(feature) {
-	  return {'color': "red", 'weight': 1.5, 'fillOpacity': 0.0}
-    },
-    onEachFeature: function(feature, layer) {
-      var popupText = "<strong>Propietario</strong>: " + feature.properties.nom_juridi + "<br>" + "<strong>Tipo de Inmueble</strong>: " + feature.properties.t_inmueb + "<br>" + "<strong>Clasificación</strong>: " + feature.properties.clasific + "<br>" + "<strong>Amenazas</strong>: " + feature.properties.r_fisica + "<br>" + "<strong>Número de finca</strong>: " + feature.properties.finca;
-      layer.bindPopup(popupText);
-    }			
-  }).addTo(mapa);
-
-  control_capas.addOverlay(terrenos, 'Terrenos del Estado');
-});
-
-// Agregar capa WMS Asentamientos Informales
+// Agregar capa WMS Asentamientos Informales MIVAH
 var capa_asentamientos_informales = L.tileLayer.wms('https://sig.mivah.go.cr/server/services/cit/AsentamientosInformales/MapServer/WmsServer?', {
   layers: 'AsentamientosInformales',
   format: 'image/png',
@@ -69,7 +89,17 @@ var capa_asentamientos_informales = L.tileLayer.wms('https://sig.mivah.go.cr/ser
 }).addTo(mapa);
 
 // Se agrega al control de capas como de tipo "overlay"
-control_capas.addOverlay(capa_asentamientos_informales, 'Asentamientos de Costa Rica WMS');
+control_capas.addOverlay(capa_asentamientos_informales, 'Asentamientos Informales de Costa Rica WMS');
 
-// Se agrega al control de capas como de tipo "overlay"
-control_capas.addOverlay(capa_localidades, 'Localidades de Costa Rica WMS');
+// Capa raster deficit habitacional MIVAH
+var capa_deficit_habitacional = L.imageOverlay("https://dvictoria2020.github.io/tarea-3/datos/deficit/DeficitHabitacional.png", 
+	[[9.9055601491738656, -84.1796468648582987], 
+	[9.9718107353507541, -84.0860167000804068]], 
+	{opacity:0.4}
+).addTo(mapa);
+control_capas.addOverlay(capa_deficit_habitacional, 'Déficit habitacional');
+
+function updateOpacityDef() {
+  document.getElementById("span-opacity-Def").innerHTML = document.getElementById("sld-opacity-Def").value;
+  capa_deficit_habitacional.setOpacity(document.getElementById("sld-opacity-Def").value);
+}
